c7e4011aa45a7cdf21d1c9224b47908056daf83e
jasper snel <jasper snel>
2017-05-08 16:27:26 +0200

    toepassing uit het hoofd.

--- src/nl/kadaster/oca/MyFirstOCAProgram.java
+++ src/nl/kadaster/oca/MyFirstOCAProgram.java
@@ -5,6 +5,7 @@ public class MyFirstOCAProgram {
 	public static void main(String[] args) {
 		System.out.println("hoi");
 		System.out.println("hallo");
+		System.out.println("Jemig de pemig");
 		
 	}
 
